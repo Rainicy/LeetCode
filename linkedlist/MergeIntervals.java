7bc53a2854cd9efb6813f41edd3591bd1c4ed1df
Rainicy <rainicy925@gmail.com>
2014-06-01 11:20:35 -0400

    Add merge intervals, but failed

---
+++ linkedlist/MergeIntervals.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        // new list of intervals
+        List<Interval> newI = new ArrayList<Interval>();
+
+        for (int i=0; i<intervals.size(); ) {
+            int start = intervals.get(i).start;
+            int end = intervals.get(i).end;
+            if ((i+1) >= intervals.size()) {    // no more interval
+                i++;
+            }
+            // starting to merge
+            for (int j=i+1; j<intervals.size(); j++) {
+                int s = intervals.get(j).start;
+                int e = intervals.get(j).end;
+                // and end
+                if (end < s) {  // cannot merge
+                    i = j;
+                    break;
+                }
+                else {
+                    // update start and end
+                    start = (start < s) ? start : s;    // choose smaller one
+                    end = (end > e) ? end : e;  // choose the bigger one
+                    i = j+1;
+                }
+            }
+            // save to the new list
+            newI.add(new Interval(start, end));
+        }
+        return newI;
+    }
+}
